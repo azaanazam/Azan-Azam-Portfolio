a6a4c3ecc1a278235f86e3510e2d8797104472ae
Azaan Azam <azanazam611@gmail.com>
2026-05-21 06:38:53 +0500

    fix mongo connection

--- config/db.js
+++ config/db.js
@@ -1,22 +1,14 @@
 const mongoose = require('mongoose');
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 async function connectDB() {
-  if (cached.conn) return cached.conn;
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URL).then((mongoose) => {
-      return mongoose;
-    });
+    console.log("✅ MongoDB Connected Successfully");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Failed:");
+    console.error(err.message);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
 
 module.exports = connectDB;
